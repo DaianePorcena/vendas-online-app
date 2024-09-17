4dbf8dc90f15f10464ba5b01d3702c3efaac669b
Daiane Porcena <daiporcenna@gmail.com>
2024-09-17 00:55:17 -0300

    feat: insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { Text, SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 24px;
+  color: blue;
+`;
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Testandoooooo!!!</Text>
+      <Text style={styles.container}>Testandoooooo!!!</Text>
+      <TextNew>Novo Texto</TextNew>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    color: 'blue',
+  },
+});
 
 export default App;
