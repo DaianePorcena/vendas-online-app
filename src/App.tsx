ea97bf99eafc9ce21de73b6071e6bd0a444f9b48
Daiane Porcena <daiporcenna@gmail.com>
2024-09-16 22:42:05 -0300

    feat: crate app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {Text, SafeAreaView } from "react-native";
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <Text>Testandoooooo!!!</Text>
+        </SafeAreaView>
+    );
+};
+
+export default App;
