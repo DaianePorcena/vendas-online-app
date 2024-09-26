beab65bcefa2f41805a9a2b8b10c65d7b4e61230
Daiane Porcena <daiporcenna@gmail.com>
2024-09-25 21:32:37 -0300

    feat: create hool useRequest

--- src/modules/login/hooks/useLogin.ts
+++ src/modules/login/hooks/useLogin.ts
@@ -1,24 +1,19 @@
 import { useState } from 'react';
 import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native';
 
-import { connectionAPIPost } from '../../../shared/functions/connection/connectionAPI';
+import { useRequest } from '../../../shared/hooks/useRequest';
 
 export const useLogin = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>('');
+  const { authRequest, errorMessage, setErrorMessage, loading, user } = useRequest();
 
+  console.log('user', user);
   const handleOnPress = async () => {
-    setLoading(true);
-    await connectionAPIPost('http://192.168.1.2:8080/auth', {
+    authRequest({
       email,
       password,
-    }).catch(() => {
-      setErrorMessage('Usuário ou senha inválidos');
     });
-    setLoading(false);
-    console.log('clicou');
   };
 
   const handleOnChangeEmail = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
