9875efd1da7e3f860f79e69a55bf8b4877ed88f5
Daiane Porcena <daiporcenna@gmail.com>
2024-09-25 22:48:23 -0300

    feat: insert hook in userReducer

--- src/modules/login/hooks/useLogin.ts
+++ src/modules/login/hooks/useLogin.ts
@@ -1,17 +1,13 @@
 import { useState } from 'react';
 import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native';
-import { useSelector } from 'react-redux';
-import { RootState } from 'src/store';
 
 import { useRequest } from '../../../shared/hooks/useRequest';
 
 export const useLogin = () => {
-  const { user } = useSelector((state: RootState) => state.userReducer);
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const { authRequest, errorMessage, setErrorMessage, loading } = useRequest();
 
-  console.log('user', user);
   const handleOnPress = async () => {
     authRequest({
       email,
