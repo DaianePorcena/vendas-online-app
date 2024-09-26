cd0cd87636c7f68d18e3af919ea5914aa79dc0da
Daiane Porcena <daiporcenna@gmail.com>
2024-09-26 00:02:01 -0300

    feat: insert modal global

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import globalReducer from './reducer/globalReducer';
 import userReducer from './reducer/userReducer';
 
 export const store = configureStore({
   reducer: {
     userReducer,
+    globalReducer,
   },
 });
 
