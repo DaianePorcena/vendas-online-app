444b4f761d2764a8e437d287ad72c05058f5ea40
Daiane Porcena <daiporcenna@gmail.com>
2024-09-29 17:49:59 -0300

    feat: insert async storage

--- src/shared/hooks/useRequest.ts
+++ src/shared/hooks/useRequest.ts
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useGlobalReducer } from '../../store/reducer/globalReducer/useGlobalReduce';
 import { useUserReducer } from '../../store/reducer/userReducer/useUserReducer';
 import { MenuUrl } from '../enums/MenuUrl.enum';
+import { setAuthorizationToken } from '../functions/connection/auth';
 import { connectionAPIPost } from '../functions/connection/connectionAPI';
 import { RequestLogin } from '../types/requestLogin';
 import { ReturnLogin } from '../types/returnLogin';
@@ -19,6 +20,7 @@ export const useRequest = () => {
     setLoading(true);
     await connectionAPIPost<ReturnLogin>('http://192.168.1.2:8080/auth', body)
       .then((result) => {
+        setAuthorizationToken(result.accessToken);
         setUser(result.user);
         reset({
           index: 0,
