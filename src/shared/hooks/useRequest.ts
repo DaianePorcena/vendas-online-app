d8c7c52e6106be021f2afb8c71c77534fba4f3f7
Daiane Porcena <daiporcenna@gmail.com>
2024-09-29 16:58:22 -0300

    feat: inser Navigation object

--- src/shared/hooks/useRequest.ts
+++ src/shared/hooks/useRequest.ts
@@ -3,12 +3,13 @@ import { useState } from 'react';
 
 import { useGlobalReducer } from '../../store/reducer/globalReducer/useGlobalReduce';
 import { useUserReducer } from '../../store/reducer/userReducer/useUserReducer';
+import { MenuUrl } from '../enums/MenuUrl.enum';
 import { connectionAPIPost } from '../functions/connection/connectionAPI';
 import { RequestLogin } from '../types/requestLogin';
 import { ReturnLogin } from '../types/returnLogin';
 
 export const useRequest = () => {
-  const { navigate } = useNavigation<NavigationProp<ParamListBase>>();
+  const { reset } = useNavigation<NavigationProp<ParamListBase>>();
   const { setUser } = useUserReducer();
   const { setModal } = useGlobalReducer();
   const [loading, setLoading] = useState<boolean>(false);
@@ -19,7 +20,10 @@ export const useRequest = () => {
     await connectionAPIPost<ReturnLogin>('http://192.168.1.2:8080/auth', body)
       .then((result) => {
         setUser(result.user);
-        navigate('Home');
+        reset({
+          index: 0,
+          routes: [{ name: MenuUrl.HOME }],
+        });
       })
       .catch(() => {
         setModal({ visible: true, title: 'Erro', text: 'Usuário ou senha inválidos' });
